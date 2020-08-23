6fc8f382703166df591485d66ce976d395a5df76
“ShibataMomoka” <momo66ssfdxdy@gmail.com>
2020-08-23 17:23:13 +0900

    make it better

--- header.php
+++ header.php
@@ -4,7 +4,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>mysite</title>
     <!-- BootstrapのCSS読み込み -->
-    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/bootstrap.css" rel="stylesheet">
     <link href="style.css?v=<?php echo filemtime('style.css') ?>" rel="stylesheet">
     <!-- jQuery読み込み -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
